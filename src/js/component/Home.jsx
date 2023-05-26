e257149aa4842cfbc683309343dad604edf30641
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-26 17:10:41 +0000

    commit

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SecondsCounter from "./SecondCounter.jsx";
+
+//create your first component
+const Home = () => {
+  return (
+    <div>
+     <SecondsCounter />
+     </div>
+  );
+};
+
+export default Home;
